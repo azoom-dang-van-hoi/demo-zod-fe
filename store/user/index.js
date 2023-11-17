5d382f55f9fb2dbf40266ebf3dfb9c9e39039793
azoom-doan-trong-nam <doan.trong.nam@azoom.jp>
2023-11-17 09:49:30 +0700

    :+1: Add action createUser

--- store/user/index.js
+++ store/user/index.js
@@ -15,4 +15,8 @@ export const actions = {
     const data = await apis.ferry.getUser({ params: { id: 1 } }).json()
     commit('SET_USER', data)
   },
+  async createUser({ commit }, payload) {
+    const data = await apis.ferry.createUser(payload).json()
+    commit('SET_USER', data)
+  },
 }
